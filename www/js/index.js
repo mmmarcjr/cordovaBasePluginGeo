0f2e10ba1832b816f8a7eaedfd4a7233d386fef5
mmmarcjr <mmmarcjr@gmail.com>
2016-11-27 16:12:14 -0500

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -4,6 +4,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 $(window).on("orientationchange",function(){
   $('#geoInfo').html("The orientation has changed!");
 	setContentScreen();
+	setMapHeight();
 });
 
 function showLoading()
@@ -28,15 +29,12 @@ function onDeviceReady()
 
 function setContentScreen()
 {
-  $('#geoInfo').html("setContentScreen");
 	var screen = $.mobile.getScreenHeight();
 	var header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight();
 	var footer = $(".ui-footer").hasClass("ui-footer-fixed") ? $(".ui-footer").outerHeight() - 1 : $(".ui-footer").outerHeight();
 	/* content div has padding of 1em = 16px (32px top+bottom). This step can be skipped by subtracting 32px from content var directly. */
 	var contentCurrent = $(".ui-content").outerHeight() - $(".ui-content").height();
 	var content = screen - header - footer - contentCurrent;
-	$(".ui-content").height(content);
-  $('#geoInfo').html($(".ui-content").height());
 }
 
 
